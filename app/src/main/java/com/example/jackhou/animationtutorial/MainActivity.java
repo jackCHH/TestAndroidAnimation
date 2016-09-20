41cbffcf5a0da7306fa430235b084c77208bbed8
Jack Hou <jackhou@Jacks-MacBook-Pro.local>
2016-09-20 11:17:19 -0400

    Start Animation in MyActivity

--- app/src/main/java/com/example/jackhou/animationtutorial/MainActivity.java
+++ app/src/main/java/com/example/jackhou/animationtutorial/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jackhou.animationtutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView icon = (ImageView) findViewById(R.id.icon);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate_clockwise);
+        icon.startAnimation(animation);
+
+
     }
 }
